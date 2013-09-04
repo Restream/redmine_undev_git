aff92b0a5008e9da919b8f98a40e23d016bda90a
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-09-04 19:16:32 +0400

    implemented changing assigned_to and custom fields by hook

--- app/models/hook_base.rb
+++ app/models/hook_base.rb
@@ -49,21 +49,49 @@ class HookBase < ActiveRecord::Base
     # the issue may have been updated
     issue.reload
 
-    # do not update if there are no actual changes
-    return if (status.nil? || issue.status == status) &&
-              (done_ratio.nil? || issue.done_ratio == done_ratio)
+    return unless has_changes_for_issue?(issue)
 
     issue.init_journal(
         changeset.user || User.anonymous,
         ll(Setting.default_language, :text_changed_by_changeset_hook, changeset.full_text_tag(issue.project))
     )
-    issue.status = status if status
-    issue.done_ratio = done_ratio if done_ratio
+
+    change_issue(issue)
+
     Redmine::Hook.call_hook(:model_changeset_scan_commit_for_issue_ids_pre_issue_update,
                             { :changeset => changeset, :issue => issue, :hook => self })
     unless issue.save
       logger.warn("Issue ##{issue.id} could not be saved by changeset #{changeset.id}: #{issue.errors.full_messages}") if logger
     end
     issue
+  end
+
+  private
+
+  def has_changes_for_issue?(issue)
+    (status && issue.status != status) ||
+        (done_ratio.present? && issue.done_ratio != done_ratio) ||
+        (assigned_to && issue.assigned_to != assigned_to) ||
+        has_custom_field_changes_for_issue?(issue)
+  end
+
+  def has_custom_field_changes_for_issue?(issue)
+    issue.custom_field_values.inject(false) do |has_changes, cfvalue|
+      hook_value = custom_value_for(cfvalue.custom_field)
+      has_changes || (hook_value.value.present? && cfvalue.value != hook_value.value)
+    end
+  end
+
+  def change_issue(issue)
+    issue.status = status if status
+    issue.done_ratio = done_ratio if done_ratio.present?
+    issue.assigned_to = assigned_to if assigned_to
+
+    cfvalues = {}
+    issue.custom_field_values.each do |cfvalue|
+      hook_value = custom_value_for(cfvalue.custom_field)
+      cfvalues[cfvalue.custom_field_id] = hook_value.value if hook_value.value.present?
+    end
+    issue.custom_field_values = cfvalues unless cfvalues.empty?
   end
 end

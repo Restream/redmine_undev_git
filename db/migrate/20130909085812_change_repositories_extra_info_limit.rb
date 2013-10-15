d1e21ca553043e99756b9399e78e947dc61fb2e3
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-10-15 13:55:39 +0400

    change limit on repositories.extra_info column only for MySQL database

--- db/migrate/20130909085812_change_repositories_extra_info_limit.rb
+++ db/migrate/20130909085812_change_repositories_extra_info_limit.rb
@@ -1,5 +1,9 @@
 class ChangeRepositoriesExtraInfoLimit < ActiveRecord::Migration
   def change
-    change_column :repositories, :extra_info, :text, :limit => 16777215
+    if defined?(ActiveRecord::ConnectionAdapters::MysqlAdapter) &&
+        ActiveRecord::Base.connection.instance_of?(
+            ActiveRecord::ConnectionAdapters::MysqlAdapter)
+      change_column :repositories, :extra_info, :text, :limit => 16777215
+    end
   end
 end
